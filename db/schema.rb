acc1a4178d590e1f77f5338d7cdfa4cd0f4a8163
MNisarAli <dr.nisaralig@gmail.com>
2023-08-09 14:41:31 +0500

    Remove unrequired age column using rails g

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_09_093625) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_09_094022) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,7 +22,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_09_093625) do
     t.datetime "updated_at", null: false
     t.string "address"
     t.integer "dob"
-    t.integer "age"
   end
 
 end
