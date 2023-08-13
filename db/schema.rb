f70f31e375a81ec820a95d9ad9c6778091c1d579
MNisarAli <dr.nisaralig@gmail.com>
2023-08-13 17:23:30 +0500

    Remove address from students

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_13_120521) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_13_122035) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,7 +28,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_13_120521) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "date_of_birth"
-    t.string "address"
     t.string "contact"
   end
 
