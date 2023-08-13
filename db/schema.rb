02ec3317ee3ce04c7e4bffb0d70cd6b008330f06
MNisarAli <dr.nisaralig@gmail.com>
2023-08-13 16:09:58 +0500

    Remove attribute age from students

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_13_102949) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_13_103223) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,7 +30,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_13_102949) do
     t.date "dob"
     t.string "address"
     t.integer "contact"
-    t.integer "age"
   end
 
 end
