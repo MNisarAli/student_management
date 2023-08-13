3b46ae66119cc1f7d14ed9d1ac8d99c413679523
MNisarAli <dr.nisaralig@gmail.com>
2023-08-13 17:26:09 +0500

    Add local & permanent address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_13_122035) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_13_122325) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_13_122035) do
     t.datetime "updated_at", null: false
     t.date "date_of_birth"
     t.string "contact"
+    t.string "local_address"
+    t.string "permanent_address"
   end
 
 end
