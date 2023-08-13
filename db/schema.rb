25db609de899a6190fbdb5851a8da949616f5a44
MNisarAli <dr.nisaralig@gmail.com>
2023-08-13 17:30:20 +0500

    Add permanent & alternate contact number

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_13_122325) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_13_122635) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,9 +28,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_13_122325) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "date_of_birth"
-    t.string "contact"
+    t.string "permanent_contact_number"
     t.string "local_address"
     t.string "permanent_address"
+    t.string "alternate_contact_number"
   end
 
 end
