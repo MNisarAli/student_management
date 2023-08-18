e799e4ee9199b1f163d809c59a67705b6f11c269
MNisarAli <dr.nisaralig@gmail.com>
2023-08-18 23:21:09 +0500

    Create db:schema for blogs model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_13_122635) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_18_180335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "student_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["student_id"], name: "index_blogs_on_student_id"
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
@@ -34,4 +43,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_13_122635) do
     t.string "alternate_contact_number"
   end
 
+  add_foreign_key "blogs", "students"
 end
