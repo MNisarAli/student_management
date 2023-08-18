70a0b571e2545827d2f50fcf3465d84dbfbae5bd
MNisarAli <dr.nisaralig@gmail.com>
2023-08-18 23:12:00 +0500

    Add association of blogs with studnet

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < ApplicationRecord
+  has_many :blogs, dependent: :destroy
   validates :first_name, :last_name, :email, :date_of_birth, :permanent_address, :permanent_contact_number, presence: true
 end
