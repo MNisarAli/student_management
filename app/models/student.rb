628bf5fe235c9cbf0feb24ef3be57df8d02f8a92
MNisarAli <dr.nisaralig@gmail.com>
2023-08-18 23:49:51 +0500

    Add associations b/w courses and studnets

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,6 @@
 class Student < ApplicationRecord
   has_many :blogs, dependent: :destroy
+  has_and_belongs_to_many :courses
+  
   validates :first_name, :last_name, :email, :date_of_birth, :permanent_address, :permanent_contact_number, presence: true
 end
