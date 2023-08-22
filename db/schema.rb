327f19fccdaa4751aa0ed88d7b9b0ff1b97fd26c
MNisarAli <dr.nisaralig@gmail.com>
2023-08-22 15:57:39 +0500

    Create schema for student_projects table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_22_104155) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_22_105300) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_104155) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "student_projects", force: :cascade do |t|
+    t.bigint "student_id", null: false
+    t.bigint "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_student_projects_on_project_id"
+    t.index ["student_id"], name: "index_student_projects_on_student_id"
+  end
+
   create_table "students", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -58,4 +67,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_104155) do
   end
 
   add_foreign_key "blogs", "students"
+  add_foreign_key "student_projects", "projects"
+  add_foreign_key "student_projects", "students"
 end
