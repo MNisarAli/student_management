f0e3539060308c02de16b8af3bb6ed9bd50287a6
MNisarAli <dr.nisaralig@gmail.com>
2023-08-22 16:03:25 +0500

    Add associations b/w student & project

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,8 @@
 class Student < ApplicationRecord
   has_many :blogs, dependent: :destroy
   has_and_belongs_to_many :courses
+  has_many :student_projects, dependent: :destroy
+  has_many :projects, through: :student_projects
   
   validates :first_name, :last_name, :email, :date_of_birth, :permanent_address, :permanent_contact_number, presence: true
 end
